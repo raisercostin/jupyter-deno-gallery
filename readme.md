b53982b53e9b055d142e855f6f17e9d43f830779
raisercostin <raisercostin@gmail.com>
2025-02-02 00:52:30 +0200

    improve

--- readme.md
+++ readme.md
@@ -9,8 +9,15 @@
 - sample notebooks https://github.com/rgbkrk/display.js/tree/main/test-notebooks
 - showing libraries
 - https://vega.github.io/
+  - https://vega.github.io/vega/examples/
 - polars: DataFrames vs Records
   - https://pola-rs.github.io/nodejs-polars/ - serverside deno/node
   - https://github.com/pola-rs/js-polars - clientside
 - jupyter python plog galery that can be adapted easily - https://observablehq.com/@observablehq/plot-gallery
 - running arbitrary widgets - https://github.com/rgbkrk/denotebooks/discussions/4#discussioncomment-9473722
+- evolution
+  - https://vda-lab.github.io/visualisation-tutorial/index.html
+  - https://web.archive.org/web/20241104085242/https://blog.ericmarty.com/the-d3-/-vega-stack/
+  - https://github.com/vega/lyra/wiki
+  - https://observablehq.com/@d3/lets-make-a-bar-chart
+  - https://vallandingham.me/make_a_barchart_with_lyra.html
--- utils.ts
+++ utils.ts
@@ -1,6 +1,27 @@
+//import pl from "npm:nodejs-polars";
+//import { document } from "jsr:@ry/jupyter-helper";
+import * as csv from "jsr:@std/csv";
+
+export async function readAllCells2(filePath: string) {
+  const file = await Deno.readTextFile(filePath);
+  const rows = await csv.parse(file, { separator: "\t", lazyQuotes:true });
+  return rows;
+}
+export async function readAllCells(filePath: string) {
+  const file = await Deno.readTextFile(filePath);
+  return file.split("\n").map(line => line === "" ? [""] : line.split("\t"));
+}
+
+/**Row/columns are 0 based */
+export function extractRegion(rows:unknown[], option:{startRow:number, startCol:number, height?:number, width?:number, endRow?:number, endCol?:number}) {
+  const endRow = option.endRow || option.startRow+(option.height??1)
+  const endCol = option.endCol || option.startCol+(option.width??1)
+  const region = rows.slice(option.startRow, endRow+1).map(row => (row as any).slice(option.startCol,endCol+1));
+  return region;
+}
 export function isEven(n: number): boolean {
   if (n % 2 != 0) {
-      return false
+    return false
   }
   return true;
 }
