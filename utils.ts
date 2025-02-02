fa1cf8139f67934a891e78398927a45f1c434453
raisercostin <raisercostin@gmail.com>
2025-02-02 13:50:48 +0200

    worked export to png

--- utils.ts
+++ utils.ts
@@ -6,7 +6,7 @@ import * as vegalite from "npm:vega-lite";
 
 export async function readAllCells2(filePath: string) {
   const file = await Deno.readTextFile(filePath);
-  const rows = await csv.parse(file, { separator: "\t", lazyQuotes:true });
+  const rows = await csv.parse(file, { separator: "\t", lazyQuotes: true });
   return rows;
 }
 export async function readAllCells(filePath: string) {
@@ -15,10 +15,10 @@ export async function readAllCells(filePath: string) {
 }
 
 /**Row/columns are 0 based */
-export function extractRegion(rows:unknown[], option:{startRow:number, startCol:number, height?:number, width?:number, endRow?:number, endCol?:number}) {
-  const endRow = option.endRow || option.startRow+(option.height??1)
-  const endCol = option.endCol || option.startCol+(option.width??1)
-  const region = rows.slice(option.startRow, endRow+1).map(row => (row as any).slice(option.startCol,endCol+1));
+export function extractRegion(rows: unknown[], option: { startRow: number, startCol: number, height?: number, width?: number, endRow?: number, endCol?: number }) {
+  const endRow = option.endRow || option.startRow + (option.height ?? 1)
+  const endCol = option.endCol || option.startCol + (option.width ?? 1)
+  const region = rows.slice(option.startRow, endRow + 1).map(row => (row as any).slice(option.startCol, endCol + 1));
   return region;
 }
 export function isEven(n: number): boolean {
@@ -28,11 +28,22 @@ export function isEven(n: number): boolean {
   return true;
 }
 
-export async function displayVegaAsSvg(plot: any) {
-  const vlSpec = plot.toSpec();
+export async function displayVega(plot: any, option: { width?: number, height?: number } | 'svg') {
+//  const vlSpec = plot.toSpec();
+  const baseSpec = plot.toSpec();
+  // Override spec dimensions when rendering as image
+  const vlSpec = option === 'svg'
+    ? baseSpec
+    : { ...baseSpec, width: option.width, height: option.height };
   const compiled = vegalite.compile(vlSpec).spec;
-  const view = new vega.View(vega.parse(compiled), { renderer: "svg" });
-  await view.runAsync();
-  const svgOutput = await view.toSVG();
-  return await Deno.jupyter.display({ "image/svg+xml": svgOutput }, { raw: true });
+  const view = await new vega.View(vega.parse(compiled), { renderer: "svg" }).runAsync();
+  if (option === 'svg') {
+    const svgOutput = await view.toSVG();
+    return await Deno.jupyter.display({ "image/svg+xml": svgOutput }, { raw: true });
+  } else {
+    // Optionally set desired width/height if not specified in the spec
+    const canvas = await view.toCanvas();
+    const webpOutput = canvas.toDataURL("image/webp", 1.0);
+    await Deno.jupyter.display({ "image/webp": webpOutput }, { raw: true });
+  }
 }
